91c8c059ccb8820a234c303ffa9038d0e5844e5e
caramelOG <52350246+caramelOG@users.noreply.github.com>
2019-08-03 19:34:46 +0700

    Add files via upload

---
+++ EXERCISE4_Pipat_k.py
@@ -0,0 +1,65 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "-----your Score-----\n",
+      "Foundation English: 90\n",
+      "General Business: 20\n",
+      "Introduction to Computer systems: 25\n",
+      "Computer Programming: 44.3\n"
+     ]
+    }
+   ],
+   "source": [
+    "f=\"Foundation English:\"\n",
+    "FE=90\n",
+    "g=\"General Business:\"\n",
+    "GB=20\n",
+    "i=\"Introduction to Computer systems:\"\n",
+    "ICS=25\n",
+    "C=\"Computer Programming:\"\n",
+    "CR=44.3\n",
+    "print(\"-----your Score-----\")\n",
+    "print(f,FE)\n",
+    "print(g,GB)\n",
+    "print(i,ICS)\n",
+    "print(C,CR)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
