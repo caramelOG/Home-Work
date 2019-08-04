f40cd158ba696f3d48fbca55caea0f68eb2eb2d0
caramelOG <52350246+caramelOG@users.noreply.github.com>
2019-08-04 20:01:21 +0700

    Add files via upload

---
+++ Exercise4_Pipat_K.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE=80
+g="General Business:"
+GB=90
+i="Introduction to Computer systems:"
+ICS=44
+C="Computer Programming:"
+CR=75.5
+print("-----your Score-----")
+print(f,FE)
+print(g,GB)
+print(i,ICS)
+print(C,CR)
