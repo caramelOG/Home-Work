dce6464e9dd5a0e082a4717e0c0e17718adb9972
caramelOG <52350246+caramelOG@users.noreply.github.com>
2019-08-04 20:04:22 +0700

    Add files via upload

---
+++ Exercise5_1_Pipat_K.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter number1"))
+num2=float(input("Enter number2"))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
